2d7d6d013ad95fa2dac8871819bacd83deb2042b
Brent Ingram <bingr001@users.noreply.github.com>
2021-10-02 12:55:19 -0700

    successfully display Tesla locations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-
+import teslaData from './data/tesla.json'
 
 import './App.css';
 
 function App() {
+
+  console.log(teslaData)
+
   return (
     <div className="App">
       <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
@@ -12,11 +15,16 @@ function App() {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+
+        {teslaData.map(tsla => (
+          <Marker 
+          key = {tsla.id}
+          position={[tsla.gps.latitude, tsla.gps.longitude]}>
+
+          </Marker>
+        ))}
+
+
       </MapContainer>
     </div>
   );
