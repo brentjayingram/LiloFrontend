985724fc9a8873212eaa6d55636df78e6a77abcf
Brent Ingram <bingr001@users.noreply.github.com>
2021-10-02 14:20:32 -0700

    tested ingesting json for locations and popups

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import teslaData from './data/tesla.json'
-
+//once we have api endpoints rename teslaData and tsla to variables more appropriate for plastic
 import './App.css';
 
 function App() {
 
   console.log(teslaData)
+  //tsla is just a variable
+  const filteredStations = teslaData.filter(tsla => tsla.address.country === "USA")
+
 
   return (
-    <div className="App">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+      <MapContainer
+        center={[34.14418154991984, -118.11822015902482]}
+        zoom={12}
+        scrollWheelZoom={true}
+      >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
-        {teslaData.map(tsla => (
-          <Marker 
-          key = {tsla.id}
-          position={[tsla.gps.latitude, tsla.gps.longitude]}>
-
+        {filteredStations.map((tsla) => (
+          <Marker
+            key={tsla.id} position={[tsla.gps.latitude, tsla.gps.longitude]}>
+            <Popup position={[tsla.gps.latitude, tsla.gps.longitude]}>
+              <div>
+                <h2>{"Name: " + tsla.name}</h2>
+                <p>{"Status: " + tsla.status}</p>
+                <p>{"Number of Charging Stations: " + tsla.stallCount}</p>
+              </div>
+            </Popup>
           </Marker>
         ))}
 
 
       </MapContainer>
-    </div>
   );
 }
 
